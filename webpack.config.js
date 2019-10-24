c9855c37ddb5a6f0efbde78ef3ea0b22d3309537
Aliaksandr Kazerskii <sashfob@mail.ru>
2019-10-25 00:10:44 +0300

    Add initial files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool:'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
